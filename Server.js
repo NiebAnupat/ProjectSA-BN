a5c5b373e4d204cf146aeda3915c1dd2c89683d7
Nieb Anupat <anupat.k64@rsu.ac.th>
2022-11-29 04:21:34 +0700

    Add Setting Project

---
+++ Server.js
@@ -0,0 +1,8 @@
+import express from "express";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
